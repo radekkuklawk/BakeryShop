476be2bea2d40dfa21d6f20eb356c90140b050c3
radekkuklawk <68395735+radekkuklawk@users.noreply.github.com>
2020-08-23 14:41:35 +0200

    Add new class ChocoladeDecorator

---
+++ BakeryShop/ChocoladeDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryShop
+{
+    class ChocoladeDecorator : CakeDecorator
+    {
+        public ChocoladeDecorator (Cake cake) : base(cake)
+        {
+
+        }
+        public override double CalculateCost()
+        {
+            return base.CalculateCost() + 5.15;
+        }
+
+        public override string GetName()
+        {
+            return base.GetName() + " Chocolade";
+        }
+    }
+}
