f67f2a48fb71777f6421ad2998e5d64dfd2dc8d9
radekkuklawk <68395735+radekkuklawk@users.noreply.github.com>
2020-08-23 14:21:22 +0200

    add  a new abstract class Cake

---
+++ BakeryShop/Cake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryShop
+{
+    public abstract class Cake
+    {
+        private string name;
+
+        public string Name { get => name; protected set => name = value; }
+        public string Taste { get;}
+        public bool Gluten { get;}
+        public double Price { get; set;}
+
+
+        public abstract double CalculateCost();
+
+        public abstract string GetName();
+    
+
+}
+}
