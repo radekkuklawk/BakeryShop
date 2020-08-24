76e90e5f0027261d4d205d7ebd4f3659f59e2f58
radekkuklawk <68395735+radekkuklawk@users.noreply.github.com>
2020-08-24 11:43:29 +0200

    Add new class VanillaDecorator

---
+++ BakeryShop/VanillaDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryShop
+{
+    class VanillaDecorator : CakeDecorator
+    {
+        public VanillaDecorator(string name, string taste, bool gluten, double price) : base(name, taste, gluten, price)
+        {
+
+        }
+        public override double CalculateCost()
+        {
+            return base.CalculateCost() + 2.15;
+        }
+
+        public override string GetName()
+        {
+            return base.GetName() + "Vanilla";
+        }
+    }
+}
